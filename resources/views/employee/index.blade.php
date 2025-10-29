ec961471a24a1a6364aa2021d625ccbc88a84117
eka-wariah <ekawariah877@gmail.com>
2025-10-29 22:20:53 +0700

    add index

---
+++ resources/views/employee/index.blade.php
@@ -0,0 +1,105 @@
+@extends('employee.master')
+
+@push('link')
+    <link rel="stylesheet" href="{{ asset('assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+@endpush
+
+@section('title')
+    SITAW | Daftar Jurusan
+@endsection
+
+@section('content')
+    <div class="datatables">
+        <div class="card bg-info-subtle shadow-none position-relative overflow-hidden mb-4">
+            <div class="card-body px-4 py-3">
+              <div class="row align-items-center">
+                <div class="col-9">
+                  <h4 class="fw-semibold mb-8">JURUSAN</h4>
+                  <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                    <li class="breadcrumb-item" aria-current="page">Daftar Jurusan</li>
+                      <li class="breadcrumb-item">
+                        <a class="text-muted text-decoration-none" href="/administration/major/create">Tambah Jurusan</a>
+                      </li>
+                      <li class="breadcrumb-item">
+                        <a class="text-muted text-decoration-none" href="/administration/major/{id}/edit">Edit Jurusan</a>
+                      </li>
+                    </ol>
+                   
+                  </nav>
+                </div>
+                <div class="col-3">
+                  <div class="text-center mb-n5">
+                    <img src="{{ asset('assets/images/breadcrumb/ChatBc.png')}}" alt="modernize-img" class="img-fluid mb-n4" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+       
+        <div class="card">
+            <div class="card-body">
+                <div class="mb-5 position-relative">
+                    <h4 class="card-title mb-0">Daftar Jurusan</h4>
+                    <a href="/administration/major/create" class="btn btn-primary position-absolute top-0 end-0">Tambah Kategori</a>
+                </div>
+                <p class="card-subtitle mb-3">
+                    
+                </p>
+                <div class="table-responsive">
+                    <table id="file_export" class="table w-100 table-striped table-bordered display text-nowrap">
+                        <thead>
+                            <!-- start row -->
+                            <tr>
+                                <th width="10%">No</th>
+                                <th>Nama Tugas</th>
+                                <th>Jenis Tugas</th>
+                                <th>Tugas</th>
+                                <th>Tenggat Waktu</th>
+                                <th>Aksi</th>
+                                
+                            </tr>
+                            <!-- end row -->
+                        </thead>
+                        <tbody>
+                            <!-- start row -->
+                            
+                            <!-- end row -->
+                            
+                        </tbody>
+                        <tfoot>
+                            <!-- start row -->
+                            
+
+                            <tr>
+                                <th width="10%">No</th>
+                                <th>Nama Tugas</th>
+                                <th>Jenis Tugas</th>
+                                <th>Tugas</th>
+                                <th>Tenggat Waktu</th>
+                                <th>Aksi</th>
+                            </tr>
+                            <!-- end row -->
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+@endsection
+
+
+
+@push('script')
+    <script src="{{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+
+    <script src="{{ asset('assets/js/datatable/datatable-advanced.init.js') }}"></script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/employee/index', function () {
+    return view('employee.index');
+});
