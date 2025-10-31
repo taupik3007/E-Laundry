793d1f7f4a8ed909c84d349cadfc7cead54a615b
raneuaprianti <raneunagano@gmail.com>
2025-10-31 22:50:57 +0700

    payment

--- database/migrations/2025_10_31_145011_create_orders_table.php
+++ database/migrations/2025_10_31_145011_create_orders_table.php
@@ -18,6 +18,16 @@ return new class extends Migration
             $table->bigInteger('adr_phone_number');
             $table->string('adr_subdistrict');
             $table->string('adr_adress_detail');
+            $table->bigInteger('ord_packages_id');
+            $table->string('ord_pickup_address');
+            $table->string('ord_pickup_schedule');
+            $table->string('ord_delivery_schedule');
+            $table->bigInteger('ord_total_weight');
+            $table->boolean('ord_status');
+            $table->bigInteger('ord_total_price');
+            $table->bigInteger('ord_pickup_courier_id');
+            $table->bigInteger('ord_delivery_courier_id');
+            $table->string('ord_order_code');
             $table->timestamps();
             $table->renameColumn('updated_at', 'adr_updated_at');
             $table->renameColumn('created_at', 'adr_created_at');
