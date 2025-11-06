de1e5e721571a52cc52ac23b46c8854a9f820e74
eka-wariah <ekawariah877@gmail.com>
2025-11-06 23:20:45 +0700

    Save local changes before pull

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Customer\OrderLaundryController;
 use App\Http\Controllers\Employee\CustomerController;
+use App\Http\Controllers\Employee\ExpenditureController;
 use App\Http\Controllers\Employee\OrderController;
+use App\Http\Controllers\Employee\PickUpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleController;
@@ -44,10 +46,18 @@ Route::get('/employee/ordering/{id}/edit', [OrderController::class, 'edit'])->na
 Route::get('/employee/ordering/history', [OrderController::class, 'history'])->name('order.history');
 Route::get('/employee/ordering/{id}/detail', [OrderController::class, 'detail'])->name('order.detaill');
 
+Route::get('/employee/expenditure', [ExpenditureController::class, 'index'])->name('expenditure.index');
+Route::get('/employee/expenditure/create', [ExpenditureController::class, 'create'])->name('expenditure.create');
+
+Route::get('/employee/pick-up', [PickUpController::class, 'index'])->name('pickup.index');
+Route::get('/employee/pick-up/create', [PickUpController::class, 'create'])->name('pickup.create');
+
+
 Route::get('/owner/employee', [EmployesController::class, 'index'])->name('employee.index');
 Route::get('/owner/employee/create', [EmployesController::class, 'store'])->name('employee.create');
 Route::get('/owner/employee/{id}/edit', [EmployesController::class, 'edit'])->name('employee.edit');
 Route::put('/owner/employee/{id}', [EmployesController::class, 'update'])->name('employee.update');
 
 Route::get('/customer/laundry-order', [OrderLaundryController::class, 'index'])->name('laundry-order.index');
+Route::get('/customer/laundry-order/{id}/detail', [OrderLaundryController::class, 'detail'])->name('laundry-order.detaill');
 
