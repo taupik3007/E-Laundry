e4805e55cd44cc62b5713fea881c2005b7641b48
taupik3007 <taupikpathurrohman@gmail.com>
2025-11-12 11:13:20 +0700

    fix static page create employee

--- resources/views/owner/employee/create.blade.php
+++ resources/views/owner/employee/create.blade.php
@@ -1,91 +1,106 @@
 @extends('owner.master')
 
 @push('link')
-    <link rel="stylesheet" href="{{ asset('assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css') }}">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+    
 @endpush
 
 @section('title')
-    SITAW | Tambah Pegawai
+    E-Laundry Garut | Tambah Pesanan
 @endsection
 
 @section('content')
-    <div class="datatables">
-        <div class="card bg-info-subtle shadow-none position-relative overflow-hidden mb-4">
-            <div class="card-body px-4 py-3">
-              <div class="row align-items-center">
-                <div class="col-9">
-                  <h4 class="fw-semibold mb-8">Tambah Pegawai</h4>
-                  
-                </div>
-                <div class="col-3">
-                  <div class="text-center mb-n5">
-                    <img src="{{ asset('assets/images/breadcrumb/ChatBc.png')}}" alt="modernize-img" class="img-fluid mb-n4" />
-                  </div>
-                </div>
+   <div class="row">
+    <div class="col-lg-12">
+      <div class="card bg-info-subtle shadow-none position-relative overflow-hidden mb-4">
+        <div class="card-body px-4 py-3">
+          <div class="row align-items-center">
+            <div class="col-9">
+                <h4 class="fw-semibold mb-8">Tambah Pegawai</h4>
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                  <li class="breadcrumb-item">
+                    <a  class="text-muted text-decoration-none" href="/owner/employee">Daftar Pegawai</a>
+                </li>
+                    <li class="breadcrumb-item">
+                      <a class="text-muted text-decoration-none">Tambah Pegawai</a>
+                    </li>
+                  </ol>
+                 
+                </nav>
+              </div>
+            <div class="col-3">
+              <div class="text-center mb-n5">
+                <img src="{{ asset('assets/images/breadcrumb/ChatBc.png')}}" alt="modernize-img" class="img-fluid mb-n4" />
               </div>
             </div>
           </div>
-       
-    <div class="card">
-        <div class="card-body">
-            <form action="{{ route('employee.store') }}" method="POST">
-                @csrf
-
-                <div class="form-group">
-                  <label for="nama">Nama</label>
-                  <input type="text" name="nama" class="form-control" required>
-                </div>
-
-                <div class="form-group">
-                  <label for="tempat_lahir">Tempat Lahir</label>
-                  <input type="tempat_lahir" name="tempat_lahir" class="form-control" required>
-                </div>
-
-                <div class="form-group">
-                  <label for="tanggal_lahir">Tanggal Lahir</label>
-                  <input type="date" name="tanggal_lahir" class="form-control">
-                </div>
-
-                <div class="form-group">
-                  <label for="jenis_kelamin">Jenis Kelamin</label>
-                  <select name="jenis_kelamin" class="form-control">
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>
-                  </select>
-                </div>
-
-                <div class="form-group">
-                  <label for="agama">Agama</label>
-                  <select name="agama" class="form-control">
-                    <option value="Islam">Islam</option>
-                    <option value="Kristen">Kristen</option>
-                    <option value="Hindu">Hindu</option>
-                    <option value="Budha">Budha</option>
-                    <option value="Konghucu">Konghucu</option>
-                  </select>
-                </div>
-
-                <div class="form-group">
-                  <label for="alamat">Alamat</label>
-                  <textarea name="alamat" class="form-control"></textarea>
-                </div>
-            </form>
         </div>
-    </div>
-</div>
+      </div>
+        <div class="card">
+          <div class="px-4 py-3 border-bottom">
+            <h4 class="card-title mb-0">Tambah Pegawai </h4>
+          </div>
+          <form action="" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="card-body">
+                <div class="mb-4 row align-items-center">
+                    <label for="exampleInputText1" class="form-label col-sm-3 col-form-label">Nama</label>
+                    <div class="col-sm-9">
+                      <input type="text" name="usr_name" class="form-control" id="exampleInputText1"  required oninvalid="this.setCustomValidity('Nama Jurusan Wajib Diisi')" 
+                      onchange="this.setCustomValidity('')">
+                    </div>
+                    @error('usr_name')
+                      <div>error</div>
+                    @enderror
+                  </div>
+                <div class="mb-4 row align-items-center">
+                    <label for="exampleInputText1" class="form-label col-sm-3 col-form-label">Email</label>
+                    <div class="col-sm-9">
+                      <input type="text" name="email" class="form-control" id="exampleInputText1"  required oninvalid="this.setCustomValidity('Nama Jurusan Wajib Diisi')" 
+                      onchange="this.setCustomValidity('')">
+                    </div>
+                    @error('email')
+                      <div>error</div>
+                    @enderror
+                  </div>
+                  <div class="mb-4 row align-items-center">
+                    <label for="exampleInputText1" class="form-label col-sm-3 col-form-label">No. Telepon</label>
+                    <div class="col-sm-9">
+                      <input type="telp" name="usr_telephone" class="form-control" id="exampleInputText1"  required oninvalid="this.setCustomValidity('Nama Jurusan Wajib Diisi')" 
+                      onchange="this.setCustomValidity('')">
+                    </div>
+                    @error('usr_telephone')
+                      <div>error</div>
+                    @enderror
+                  </div>
+                  <div class="mb-4 row align-items-center">
+                    <label for="exampleInputText1" class="form-label col-sm-3 col-form-label">Password</label>
+                    <div class="col-sm-9">
+                      <input type="password" name="password" class="form-control" id="exampleInputText1"  required oninvalid="this.setCustomValidity('Nama Jurusan Wajib Diisi')" 
+                      onchange="this.setCustomValidity('')">
+                    </div>
+                    @error('password')
+                      <div>error</div>
+                    @enderror
+                  </div>
+                <div class="row">
+                  <div class="col-sm-3"></div>
+                  <div class="col-sm-9">
+                    <input type="submit" class="btn btn-primary" value="Kirim" id="">
+                    <a href="/employee/expenditure" class="btn btn-warning">Batal</a>
+                  </div>
+                </div>
+              </div>
+          </form>
+          
+        </div>
+      </div>
+   </div>
+    
 @endsection
 
 
 
 @push('script')
-    <script src="{{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
-
-    <script src="{{ asset('assets/js/datatable/datatable-advanced.init.js') }}"></script>
+    
 @endpush
