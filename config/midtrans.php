325725d8abb8f92700feca5ff917f8db1f2e79ee
taupik3007 <taupikpathurrohman@gmail.com>
2025-11-26 23:25:01 +0700

    fix ordering index

--- config/midtrans.php
+++ config/midtrans.php
@@ -4,4 +4,7 @@ return [
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'server_key'    => env('MIDTRANS_SERVER_KEY'),
     'client_key'    => env('MIDTRANS_CLIENT_KEY'),
+    'merchant_id'   => env('MIDTRANS_MERCHANT_ID'),
+    'is_sanitized'  => true,
+    'is_3ds'        => true,
 ];
