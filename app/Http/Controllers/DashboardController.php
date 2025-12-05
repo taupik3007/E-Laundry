bfa493b7203eb8762633fe7784c4d28fecfda187
taupik3007 <taupikpathurrohman@gmail.com>
2025-12-05 23:28:21 +0700

    continue employee dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers;
 use App\Models\LaundryPackage;
 use App\Models\LaundryService;
+use App\Models\Order;
+use App\Models\Payment;
+
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     function employeeDashboard(){
         $service = LaundryService::count();
+        $order = Order::with(['service', 'package'])
+    ->whereNotIn('ord_status', ['selesai', 'dibatalkan'])
+    ->count();
+    $orderDone = Order::with(['service', 'package'])
+    ->whereNotIn('ord_status', ['dibatalkan'])
+    ->count();
+    $todaySales = Payment::where('pym_payment_status', 1)
+    ->whereDate('pym_created_at', today())
+    ->sum('pym_amount');
+
         // dd($service);
-        return view('employee.dashboard',compact(['service']));
+        $member = User::role('customer')->count();
+        return view('employee.dashboard',compact(['service','order','orderDone','member','todaySales']));
     }
 }
