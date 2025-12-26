4adf0cc0dc2e47e67c4bbaef0582b04c2eab8ac4
taupik3007 <taupikpathurrohman@gmail.com>
2025-12-26 22:33:50 +0700

    fix scales button

--- resources/views/employee/order-laundry/index.blade.php
+++ resources/views/employee/order-laundry/index.blade.php
@@ -236,10 +236,10 @@
                                             data-icon="ic:baseline-balance"
                                             data-width="18"></span>
                                     </button>
-                                            <button class="btn btn-info" data-bs-toggle="modal"
+                                            {{-- <button class="btn btn-info" data-bs-toggle="modal"
                                                 data-bs-target="#modalTimbang{{ $order->ord_id }}">
                                                 Timbang
-                                            </button>
+                                            </button> --}}
                                         @endif
 
                                         {{-- AKSI --}}
